4224d0786d8a36e4b577705269cd285cdf987633
Josiahalabi <josiahalabi@gmail.com>
2023-10-18 02:51:05 -0700

    fourth

--- growth_fun.c
+++ growth_fun.c
@@ -1,34 +1,34 @@
 #include "main.h"
 
 /**
- * get_flags - Calculates active flags
- * @format: Formatted string in which to print the arguments
- * @i: take a parameter.
+ * get_flags - Calculates active flags as done previously
+ * @format: Formatted string in which to print the arguments and function
+ * @n: must be given a parameter
  * Return: Flags:
  */
-int get_flags(const char *format, int *i)
+int get_flags(const char *format, int *n)
 {
 	/* - + 0 # ' ' */
 	/* 1 2 4 8  16 */
-	int j, curr_i;
+	int a, curr_n;
 	int flags = 0;
 	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
 	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
 
-	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
+	for (curr_n = *n + 1; format[curr_n] != '\0'; curr_n++)
 	{
-		for (j = 0; FLAGS_CH[j] != '\0'; j++)
-			if (format[curr_i] == FLAGS_CH[j])
+		for (a = 0; FLAGS_CH[a] != '\0'; a++)
+			if (format[curr_n] == FLAGS_CH[a])
 			{
-				flags |= FLAGS_ARR[j];
+				flags |= FLAGS_ARR[a];
 				break;
 			}
 
-		if (FLAGS_CH[j] == 0)
+		if (FLAGS_CH[a] == 0)
 			break;
 	}
 
-	*i = curr_i - 1;
+	*n = curr_n - 1;
 
 	return (flags);
 }
